01c9b7de3da82a35d02ab77106cb87c917572f4a
jackr276 <jack_m_robbins@me.com>
2023-12-26 11:42:47 -0500

    Create minimum_array_removal.py

---
+++ minimum_array_removal.py
@@ -0,0 +1,21 @@
+"""
+Jack Robbins
+Minimum Array Removal
+"""
+
+
+def reductionCost(num):
+    #array of all potential reduction costs
+    reductionCosts = []
+
+
+
+    return min(reductionCosts)
+
+
+def main():
+    print("Minimum cost of array removal")
+
+
+if __name__ == '__main__':
+    main()
