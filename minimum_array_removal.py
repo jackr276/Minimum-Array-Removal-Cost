e42761c6dedaa4694f15f4bd56b921abc3ae4bac
jackr276 <jack_m_robbins@me.com>
2023-12-26 12:46:54 -0500

    Update minimum_array_removal.py

--- minimum_array_removal.py
+++ minimum_array_removal.py
@@ -4,17 +4,36 @@ Minimum Array Removal
 """
 
 
-def reductionCost(num):
-    #array of all potential reduction costs
-    reductionCosts = []
+def reductionCost(startInd, endInd, num):
+    #base condition
+    if startInd >= endInd:
+        return 0
+    
+    #super high starting point
+    cost = 10000000000000
+    
+    for k in range(startInd, endInd):
 
+        #left hand side problem
+        left_cost = reductionCost(startInd, k, num)
 
+        #right hand side problem
+        right_cost = reductionCost(k+1, endInd, num)
 
-    return min(reductionCosts)
+        cost = min(cost, right_cost + left_cost + computeCost(num, startInd, endInd))
+    
+    return cost
 
+def computeCost(num, i, j):
+    cost = 0
+    for k in range(i, j+1):
+        cost += num[k]
+
+    return cost
 
 def main():
-    print("Minimum cost of array removal")
+    num = [1, 2, 3]
+    print(reductionCost(0, len(num)-1, num))
 
 
 if __name__ == '__main__':
